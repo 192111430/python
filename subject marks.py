0db009afb2d36f3bfc63d513a64753e5913cd2d7
192111430 <112375898+192111430@users.noreply.github.com>
2022-08-29 13:57:42 +0530

    Add files via upload

---
+++ subject marks.py
@@ -0,0 +1,16 @@
+s1=int(input("enter the marks of first subject:"))
+s2=int(input("enter the marks of second subject:"))
+s3=int(input("enter the marks of third subject:"))
+s4=int(input("enter the marks of fourth subject:"))
+s5=int(input("enter the marks of fifth subject:"))
+avg=(s1+s2+s3+s4+s5)/5
+if(avg>=90):
+    print('grade=S')
+elif(avg>=80):
+    print('grade=A')
+elif(avg>=70):
+    print('grade=B')
+elif(avg>=60):
+    print('grade=C')
+else:
+    print('fail')
