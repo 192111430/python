e47c65ef99c2fe639c08465b2b73937d5fb6f568
192111430 <112375898+192111430@users.noreply.github.com>
2022-09-01 10:27:16 +0530

    Add files via upload

---
+++ quadratic.py
@@ -0,0 +1,13 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/quadratic.py 
+ A= 77
+ B= 88
+ C= 43
+the solution for the equation are (-0.5714285714285714+0.4815713303308872j) and (-0.5714285714285714-0.4815713303308872j)
+>>> 77
+77
+>>> 55
+55
+>>> 
